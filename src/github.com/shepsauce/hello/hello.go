20760ccfe52131b18589e3157a40b676ce7c464d
Charles Clauss <charles.clauss@aps.edu>
2017-04-09 00:00:56 -0600

    Build first package

--- src/github.com/shepsauce/hello/hello.go
+++ src/github.com/shepsauce/hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shepsauce/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
