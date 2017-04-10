c08518ccbe5a791a8a17a495bcacdd398ab5a3c4
Charles Clauss <charles.clauss@aps.edu>
2017-04-09 20:04:09 -0600

    Solution to projecteuler 3-6

--- src/github.com/shepsauce/projecteuler/003_primefactor/main.go
+++ src/github.com/shepsauce/projecteuler/003_primefactor/main.go
@@ -1,13 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	factor := 0
 	number := 600851475143
-	for i := 3; i < number; i++ {
+	for i := 7; i < int(math.Trunc(math.Sqrt(float64(number)))); i++ {
 		if number%i == 0 {
-			factor = i
+			factor = number / i
+			for j := 3; j < factor; j++ {
+				if factor%j == 0 {
+					factor = 0
+					break
+				}
+			}
+			if factor == 0 {
+				continue
+			} else {
+				break
+			}
+		}
+	}
+	if factor == 0 {
+		for i := int(math.Trunc(math.Sqrt(float64(number)))); i > 2; i-- {
+			if number%i == 0 {
+				factor = i
+				for j := 3; j < factor; j++ {
+					if factor%j == 0 {
+						factor = 0
+						break
+					}
+				}
+				if factor != 0 {
+					break
+				}
+			}
 		}
 	}
 	fmt.Println(factor)
