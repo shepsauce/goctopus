c15a48a06465cfe8e01e3ec699ec31772902edc7
Charles Clauss <charles.clauss@aps.edu>
2017-04-10 12:54:24 -0600

    projecteuler 10 solution, reused 7

--- src/github.com/shepsauce/projecteuler/007_primes/main.go
+++ src/github.com/shepsauce/projecteuler/007_primes/main.go
@@ -1,11 +1,9 @@
-package main
+package prime
 
-import "fmt"
-
-func main() {
+func Prime(n int) []int {
 	var slice []int
 	slice = append(slice, 2, 3, 5, 7, 11, 13, 17, 19)
-	for i := 20; i < 222200; i++ {
+	for i := 20; i < n; i++ {
 		for j, v := range slice {
 			if i%v == 0 {
 				break
@@ -14,9 +12,6 @@ func main() {
 				slice = append(slice, i)
 			}
 		}
-		if len(slice) == 10001 {
-			break
-		}
 	}
-	fmt.Println(slice[10000])
+	return slice
 }
