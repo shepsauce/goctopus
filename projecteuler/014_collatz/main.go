94f65ca0dbd57c5e9cc0f0bb899598df1de71ddd
Charles Clauss <charles.clauss@aps.edu>
2017-05-03 14:04:22 -0600

    projecteuler #14

---
+++ projecteuler/014_collatz/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	chains := make(map[int]int)
+	chains[1] = 1
+	chains[2] = 2
+	for i := 3; i < 1000000; i++ {
+		if chains[i] == 0 {
+			temp := i
+			chain := make([]int, 0)
+			chain = append(chain, i)
+			chains[i] = 1
+			for {
+				if temp%2 == 0 {
+					temp /= 2
+				} else {
+					temp = 3*temp + 1
+				}
+				if length, ok := chains[temp]; ok {
+					for k, v := range chain {
+						if k == 0 {
+							chains[v] = length + len(chain)
+						} else {
+							if v%3 == 1 {
+								chains[v] = length + len(chain) - k
+							}
+						}
+					}
+					break
+				} else {
+					chain = append(chain, temp)
+				}
+			}
+		} else {
+			continue
+		}
+	}
+	var max int
+	var num int
+	for k, v := range chains {
+		if v > max {
+			max = v
+			num = k
+		}
+	}
+	fmt.Println(max, num)
+}
