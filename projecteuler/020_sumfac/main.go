18980541602be74728bb9e616aeb40a969534657
Charles Clauss <charles.clauss@aps.edu>
2017-05-30 12:57:22 -0600

    projecteuler #20

---
+++ projecteuler/020_sumfac/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	fac := big.NewInt(1)
+	for i := int64(2); i < 101; i += 1 {
+		fac.Mul(fac, big.NewInt(i))
+	}
+	mod := new(big.Int)
+	sum := big.NewInt(0)
+	ten := big.NewInt(10)
+	for {
+		fac.DivMod(fac, ten, mod)
+		sum.Add(sum, mod)
+		if less := fac.Cmp(ten); less == -1 {
+			sum.Add(sum, fac)
+			break
+		}
+	}
+	fmt.Println(sum)
+}
