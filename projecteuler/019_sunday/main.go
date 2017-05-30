404979a6398a00d209095aaf6ff377a130269a80
Charles Clauss <charles.clauss@aps.edu>
2017-05-29 21:30:42 -0600

    projecteuler #19

---
+++ projecteuler/019_sunday/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := []int{3, 0, 3, 2, 3, 2, 3, 3, 2, 3, 2, 3}
+	d := 3
+	count := 2
+	for i := 2; i <= 100; i++ {
+		for k, v := range m {
+			d += v
+			if i%4 == 0 && k == 1 {
+				d += 1
+			}
+			if d%7 == 0 {
+				count++
+			}
+		}
+	}
+	fmt.Println(count)
+}
